6cc9f2f6bd47322f58bac7d563bf8cd700193db0
Yannos95 <yann.tran@efrei.net>
2025-12-06 16:25:05 +0100

    Python path in tests for CI environment

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import os
 from datetime import date, timedelta
 from unittest.mock import patch
